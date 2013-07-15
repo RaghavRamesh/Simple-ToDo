45daf239679f17608a90a226861c8ea0cd3365b3
Raghav Ramesh <raghavrnus@gmail.com>
2013-07-16 02:18:43 +0800

    Created 'Add' button, dynamic Todo counter.

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,8 +1,18 @@
 function TodoCtrl($scope) {
-	$scope.totalTodos = 4;
+	
 
 	$scope.todos = [
 		{text:'Learn Angular', done:false},
 		{text:'Create app', done:false}
 	];
+
+	$scope.getTotalTodos = function () {
+		return $scope.todos.length;
+	}
+	
+
+	$scope.addTodo = function () {
+		$scope.todos.push({text:$scope.formTodoText, done:false});
+		$scope.formTodoText="";
+	}
 }
