715b3248cf931fd50d7b5a2cba28637ba2f288e2
Raghav Ramesh <raghavrnus@gmail.com>
2013-07-16 02:30:59 +0800

    Added 'Clear Completed' button.
    Imported filter for completed tasks and implemented to clear completed
    tasks.

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,6 +1,5 @@
 function TodoCtrl($scope) {
 	
-
 	$scope.todos = [
 		{text:'Learn Angular', done:false},
 		{text:'Create app', done:false}
@@ -8,11 +7,16 @@ function TodoCtrl($scope) {
 
 	$scope.getTotalTodos = function () {
 		return $scope.todos.length;
-	}
+	};
 	
-
-	$scope.addTodo = function () {
+	$scope.addTodo = function () {	
 		$scope.todos.push({text:$scope.formTodoText, done:false});
 		$scope.formTodoText="";
-	}
+	};
+
+	$scope.clearCompleted = function () {
+		$scope.todos = _.filter($scope.todos, function(todo){
+			return !todo.done;
+		})
+	};
 }
