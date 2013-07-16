fbee1aace69d066507a0a0977b184cfe254d6f48
Raghav Ramesh <raghavrnus@gmail.com>
2013-07-16 16:59:15 +0800

    Rendered in localhost.
    Used SimpleHTTPServer to render using Python. Modified js to match style
    guide in BillPin.

---
+++ static/js/todo-modified.js
@@ -0,0 +1,32 @@
+angular.module('ToDo', [])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: '../static/todo/todo.html',
+				controller: 'ToDo.ToDoController'
+			})
+	}])
+	.controller('ToDo.ToDoController', [
+		'$scope',
+
+		function($scope) {
+
+			$scope.todos = [
+				{text:'Learn Angular', done:false},
+				{text:'Create app', done:false}
+			];
+			$scope.getTotalTodos = function () {
+				return $scope.todos.length;
+			};	
+			$scope.addTodo = function () {	
+				$scope.todos.push({text:$scope.formTodoText, done:false});
+				$scope.formTodoText="";
+			};
+			$scope.clearCompleted = function () {
+				$scope.todos = _.filter($scope.todos, function(todo){
+					return !todo.done;
+				})
+			};
+
+		}
+	]);
